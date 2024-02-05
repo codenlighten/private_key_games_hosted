57c1c8da94918475199ab996edf84a1c0ba912ac
greg ward <codenlighten1@gmail.com>
2024-02-05 11:47:00 -0500

    first hosted

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const fetch = require("node-fetch");
 const { generateKeys } = require("./keys");
 const { addAttempt, getAttempts, connectDB } = require("./mongo");
 const cors = require("cors");
+
 app.use(cors());
-// cors addressgame.com
+// cors for addressgame.com
 app.use(cors({ origin: "https://addressgame.com" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 let attempts = [];
+
 //get attempts
 const getAttemptsMongo = async () => {
   attempts = await getAttempts();
@@ -24,10 +26,7 @@ app.get("/", (req, res) => {
 
 app.post("/api/attempt", async (req, res) => {
   const { address, guess } = req.body;
-  const attempt = {
-    address,
-    guess,
-  };
+  const attempt = { address, guess };
   await addAttempt(attempt);
   attempts.push(attempt);
   res.json({ message: "Attempt added" });
@@ -43,7 +42,18 @@ app.get("/api/attempts", async (req, res) => {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  connectDB();
-  console.log(`Server is running on port ${port}`);
-});
+// Modified to ensure MongoDB connection before starting the server
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log(`Connected to MongoDB`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(`Failed to connect to MongoDB: ${error}`);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+startServer();
